96a78470eb4afae7cb7a9b96ec937f60234aeca8
Tu NGuyen <nguyendichtu91295@gmail.com>
2021-05-08 17:44:33 +0800

    Optimize answer 2 with hashmap/object for O(1) access

--- leetcode/1_two_sum.js
+++ leetcode/1_two_sum.js
@@ -6,7 +6,7 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let result = [];
+    // 2 loops ~= brute force
     let head = 0;
     let tail = 1;
     let temp = 0;
@@ -27,7 +27,50 @@ var twoSum = function (nums, target) {
 
         temp -= nums[head];
         head += 1;
+        tail = head + 1;
+    }
+};
+
+
+var twoSumV2 = function (nums, target) {
+    // using obj/hashmap
+    const objIndex = {}
+
+    nums.forEach((item, index) => {
+        if (objIndex[item] !== undefined) {
+            if (Array.isArray(objIndex[item])) {
+                objIndex[item].push(index);
+            } else {
+                objIndex[item] = [objIndex[item], index]
+            }
+
+            return;
+        }
+
+        objIndex[item] = index;
+    })
+
+    let key = -1;
+    let value = -1;
+    let item = -1;
+
+    for(let i = 0; i < nums.length; i++) {
+        item = nums[i];
+        key = target - item;
+        value = objIndex[key];
+
+        if (value === undefined) {
+            continue;
+        }
+        
+        if (!Array.isArray(value) && i !== value) {
+            return [i, value];
+        }
+
+        if (Array.isArray(value)) {
+            return [i, value.filter(item => item != i)[0]];
+        }
     }
 
-    
+    return [];
 };

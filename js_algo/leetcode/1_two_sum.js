69a565cf7abe4f427adf0a2768ef8e13e1c60fe6
tu.nd <tu.nd@tunds-iMac.local>
2022-05-02 22:16:09 +0800

    Add eslint + prettier

--- js_algo/leetcode/1_two_sum.js
+++ js_algo/leetcode/1_two_sum.js
@@ -5,74 +5,74 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+
+const twoSum = (nums, target) => {
     // 2 loops ~= brute force
-    let head = 0;
-    let tail = 1;
-    let temp = 0;
+    let head = 0
+    let tail = 1
+    let temp = 0
 
     while (head < nums.length) {
-        temp += nums[head];
+        temp += nums[head]
 
-        while(tail < nums.length) {
-            temp = nums[head] + nums[tail];
+        while (tail < nums.length) {
+            temp = nums[head] + nums[tail]
 
             if (temp === target) {
-                return [head, tail];
-            } else {
-                temp -= nums[tail];
-                tail += 1;
+                return [head, tail]
             }
+
+            temp -= nums[tail]
+            tail += 1
         }
 
-        temp -= nums[head];
-        head += 1;
-        tail = head + 1;
+        temp -= nums[head]
+        head += 1
+        tail = head + 1
     }
-};
+}
 
-
-var twoSumV2 = function (nums, target) {
+const twoSumV2 = (nums, target) => {
     // using obj/hashmap
     const objIndex = {}
 
     nums.forEach((item, index) => {
         if (objIndex[item] !== undefined) {
             if (Array.isArray(objIndex[item])) {
-                objIndex[item].push(index);
+                objIndex[item].push(index)
             } else {
                 objIndex[item] = [objIndex[item], index]
             }
 
-            return;
+            return
         }
 
-        objIndex[item] = index;
+        objIndex[item] = index
     })
 
-    let key = -1;
-    let value = -1;
-    let item = -1;
+    let key = -1
+    let value = -1
+    let item = -1
 
-    for(let i = 0; i < nums.length; i++) {
-        item = nums[i];
-        key = target - item;
-        value = objIndex[key];
+    for (let i = 0; i < nums.length; i++) {
+        item = nums[i]
+        key = target - item
+        value = objIndex[key]
 
         if (value === undefined) {
-            continue;
+            continue
         }
-        
+
         if (!Array.isArray(value) && i !== value) {
-            return [i, value];
+            return [i, value]
         }
 
         if (Array.isArray(value)) {
-            return [i, value.filter(item => item != i)[0]];
+            return [i, value.filter((innerItem) => innerItem !== i)[0]]
         }
     }
 
-    return [];
-};
+    return []
+}
 
-export {twoSum}
+export { twoSum, twoSumV2 }

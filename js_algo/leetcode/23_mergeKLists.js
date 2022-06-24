2f1e6e9b01868ebbc1a4fb0d731951df99bb4d88
tu.nd <tu.nd@tunds-iMac.local>
2022-06-24 15:10:57 +0800

    23

--- js_algo/leetcode/23_mergeKLists.js
+++ js_algo/leetcode/23_mergeKLists.js
@@ -7,8 +7,50 @@ class ListNode {
     }
 }
 
-const mergeKLists = (ransomNote, magazine) => {
+const mergeKLists = (lists) => {
+    if (lists.length === 0) {
+        return new ListNode().next
+    }
 
+    const merge = (left, right) => {
+        const dummyNode = new ListNode()
+        let current = dummyNode
+
+        while(left && right) {
+            if (left.val < right.val) {
+                current.next = left
+                left = left.next
+            } else {
+                current.next = right
+                right = right.next
+            }
+            current = current.next
+        }
+
+        if (!left) {
+            current.next = right
+        } else if (!right) {
+            current.next = left
+        }
+
+        return dummyNode.next
+    }
+
+    const divide = (start = 0, end = lists.length - 1) => {
+        if (start >= end) {
+            return lists[start]
+        }
+
+        const mid = Math.floor((start + end) / 2)
+        const left = divide(start, mid)
+        const right = divide(mid + 1, end)
+
+        const node = merge(left, right)
+
+        return node
+    }
+
+    return divide()
 };
 
 const run = () => {
